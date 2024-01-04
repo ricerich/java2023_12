9d59e4ead92239e8b3b5c7c49d2206ab6d3ac8c5
it <it@DESKTOP-1DJROPA>
2024-01-04 12:10:11 +0900

    4장 7번

---
+++ ex02/src/ex04/MonthSchedule.java
@@ -0,0 +1,76 @@
+package ex04;
+
+import java.util.Scanner;
+
+public class MonthSchedule {
+	
+	int totalDay;
+	Scanner sc;
+	Day arr[];
+
+	public MonthSchedule(int totalDay) {
+		sc = new Scanner(System.in);
+		this.totalDay = totalDay;
+		arr = new Day[totalDay];
+	}
+
+	public void run() {
+		System.out.println("이번달 스케줄 관리 프로그램.");
+		
+		while(true)
+		{
+			System.out.print("할일(입력:1, 보기:2, 끝내기:3)>>");
+			int option = sc.nextInt();
+			
+			switch(option)
+			{
+				case 1:
+					input();
+					break;
+				case 2:
+					view();
+					break;
+				case 3:
+					finish();
+					return;
+				default:
+					System.out.println("잘못입력했어요!");
+			}
+		}
+	}
+
+	private void input() {
+		System.out.print("날짜(1~30)?");
+		int day = sc.nextInt();
+		System.out.print("할일(빈칸없이 입력)?");
+		String work = sc.next();
+		day--;
+		arr[day] = new Day();
+		arr[day].set(work);
+	}
+
+	private void view() {
+		System.out.print("날짜(1~30)?");
+		int day = sc.nextInt();
+		day--;
+		
+		System.out.println((day+1)+"일 의 할일은");
+		if(arr[day] != null)
+			arr[day].show();
+		
+	}
+
+	private void finish() {
+		sc.close();
+		System.out.println("종료합니다");		
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ ex02/src/ex04/ex07.java
@@ -0,0 +1,10 @@
+package ex04;
+
+public class ex07 
+{
+	public static void main(String[] args) {
+		MonthSchedule april = new MonthSchedule(30); // 4월달의 할 일
+		april.run();
+	}
+
+}
